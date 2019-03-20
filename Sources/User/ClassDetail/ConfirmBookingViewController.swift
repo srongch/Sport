db012960caf5cebba654fb1fb238bcb5d5e679df
Sronglong Chhem <sronglong.derby@gmail.com>
2019-03-20 01:39:23 +0000

    init

---
+++ Sources/User/ClassDetail/ConfirmBookingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ConfirmBookingViewController.swift
+//  User
+//
+//  Created by 100456065 on 20/03/2019.
+//  Copyright © 2019 100456065. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmBookingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
